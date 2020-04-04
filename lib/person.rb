d77ee6afeebb5271249503c795b0a75629cd5706
James Johnson <jtaylorjohnson@gmail.com>
2020-04-04 21:32:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
   def name 
-    name = 
+    @name 
+  end
+  
+  def name=(input)
+    @name = input
+  end
+  
+  def job 
+    @job
+  end
+  
+  def job=(input)
+    @job = input
+  end
+end
+
+  
+  
